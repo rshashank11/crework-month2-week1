e8e1a6478c09ae0797d9410475f794debbe5a196
rshashank11 <shashankyadav2000@gmail.com>
2022-08-19 21:48:58 +0530

    fixed the code to generate 5 different colors

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const btnGenerate = document.querySelector("#btn-gnrt");
 btnGenerate.addEventListener("click", colorGenerate);
 
 function colorGenerate() {
-    for (const i = 1; i <= 5; i++) {
-        let red = Math.floor(Math.random() * 255);
-        let green = Math.floor(Math.random() * 255);
-        let blue = Math.floor(Math.random() * 255);
+    for (var i = 1; i <= 5; i++) {
+        var red = Math.floor(Math.random() * 256);
+        var green = Math.floor(Math.random() * 256);
+        var blue = Math.floor(Math.random() * 256);
 
-        //var hexValue = red.toString(16) + green.toString(16) + blue.toString(16);
+        var hexValue = red.toString(16) + green.toString(16) + blue.toString(16);
+        hexValue = hexValue.toString();
 
-        let containerId = "container-" + i.toString();
-        //const hexOutputId = "hex-output-" + i.toString();
-        //const rgbOutputId = "rgb-output-" + i.toString();
+        var containerClass = ".container-" + i.toString();
+        var hexOutputClass = ".hex-output-" + i.toString();
+        var rgbOutputClass = ".rgb-output-" + i.toString();
 
-        document.getElementsByClassName(containerId).style.backgroundColor = `rgb(${red},${green},${blue})`;
+        document.querySelector(containerClass).style.backgroundColor = `rgb(${red},${green},${blue})`;
+        document.querySelector(hexOutputClass).innerText = "#" + hexValue;
+        document.querySelector(rgbOutputClass).innerText = `rgb(${red},${green},${blue})`;
     }
 }
