5d24700b7c9f77d15c36bd7ff97c5571ead147a9
rshashank11 <shashankyadav2000@gmail.com>
2022-08-21 15:23:42 +0530

    Final commit

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const btnGenerate = document.querySelector("#btn-gnrt");
+const mainSection = document.querySelector("#section-main");
+
+mainSection.style.display = "none";
 
 btnGenerate.addEventListener("click", colorGenerate);
 
+
 function colorGenerate() {
-    for (var i = 1; i <= 5; i++) {
-        var red = Math.floor(Math.random() * 256);
-        var green = Math.floor(Math.random() * 256);
-        var blue = Math.floor(Math.random() * 256);
+    mainSection.style.display = "grid";
+    for (let i = 1; i <= 5; i++) {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
 
-        var hexValue = red.toString(16) + green.toString(16) + blue.toString(16);
-        hexValue = hexValue.toString();
+        let redString = red.toString(16);
+        let greenString = green.toString(16);
+        let blueString = blue.toString(16);
 
-        var containerClass = ".container-" + i.toString();
-        var hexOutputClass = ".hex-output-" + i.toString();
-        var rgbOutputClass = ".rgb-output-" + i.toString();
+        if(redString.length < 2) {
+           redString =  redString.padStart(2, '0');
+        }
+        if(greenString.length < 2) {
+           greenString =  greenString.padStart(2, '0');
+        }
+        if(blueString.length < 2) {
+           blueString = blueString.padStart(2, '0');
+        }
+
+        let hexValue = redString + greenString + blueString;
+
+        let containerClass = ".container-" + i.toString();
+        let hexOutputClass = ".hex-output-" + i.toString();
+        let rgbOutputClass = ".rgb-output-" + i.toString();
 
         document.querySelector(containerClass).style.backgroundColor = `rgb(${red},${green},${blue})`;
         document.querySelector(hexOutputClass).innerText = "#" + hexValue;
